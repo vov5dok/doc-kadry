9792c035eeb1d662218385a94a62a3d2e84f1f2f
ivcDark <94673346+ivcDark@users.noreply.github.com>
2023-02-24 11:34:43 +0400

    Доработали миграцию для пользователей

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,13 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('fio');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('position_office');
+            $table->string('department');
+            $table->softDeletes();
             $table->rememberToken();
             $table->timestamps();
         });
